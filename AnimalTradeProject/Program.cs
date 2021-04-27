787f7cd31d56d64ca37886273a781e0cfeeb3fa3
gycsaba <gyuris.csaba@vasvari.org>
2021-04-27 14:00:31 +0200

    AnimapTradeProject létrejött

---
+++ AnimalTradeProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnimalTradeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
