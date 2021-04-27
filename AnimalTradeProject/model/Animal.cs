eb2bb0e8bffd3ee18e007baeff44a3877030077d
gycsaba <gyuris.csaba@vasvari.org>
2021-04-27 14:02:59 +0200

    I1.01 Absztrakt osztály

---
+++ AnimalTradeProject/model/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop.model
+{   
+    /// <summary>
+    /// Álat osztály
+    /// I1.01 Absztrakt osztály
+    ///       Az absztrakt osztályokból nem lehetséges a példányosítás (http://aries.ektf.hu/~hz/wiki7/mprog2ea/abstract)
+    ///       Mivel olyan a természetbe konkrétan nincs, hogy állat egy példánya, nem is akarunk állatot példányosítani
+    /// </summary>
+    abstract class Animal
+    {
+
+    }
+}
