ea10db138364f16912e745bb9a77675a12c7a458
gycsaba <gyuris.csaba@vasvari.org>
2021-04-27 14:06:25 +0200

    I1.04 Öröklődés

---
+++ AnimalTradeProject/model/Dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop.model
+{
+    /// I1.04 Öröklődés
+    ///       A kutya egy állat, tehát örököltetjük az "Animal" osztályból. Tehát bizonyos dolgokat átvesz abból az osztályból.
+    ///       https://csharp.net-tutorials.com/hu/122/osztalyok/oroklodes-/
+    /// <summary>
+    /// Kutya osztály
+    /// </summary>
+    /// 
+    class Dog : Animal
+    {
+    }
+}
